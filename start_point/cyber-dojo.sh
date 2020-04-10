e6dce8c891ae21e16faa35349b10eb8b9efda4e5
Jon Jagger <jon@jaggersoft.com>
2020-04-10 20:14:35 +0100

    Move to groovy 3.0.3

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -1,5 +1,5 @@
 
-groovyc -cp /groovy/spock-core-1.0-groovy-2.4.jar *.groovy
+groovyc -cp /groovy/spock-core-2.0-M2-groovy-3.0.jar *.groovy
 if [ $? -eq 0 ]; then
   # run test classes even if they are inner classes
   java -cp .:$(ls /groovy/*.jar | xargs | sed -e 's/ /:/g') \
---
+++ start_point/readme.txt
@@ -0,0 +1,2 @@
+Downloaded spock jar file from
+https://search.maven.org/search?q=g:org.spockframework
