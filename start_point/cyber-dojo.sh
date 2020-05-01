35dd19d986aade238e969200da32b85c72ae9897
Jon Jagger <jon@jaggersoft.com>
2020-05-01 13:29:07 +0100

    Switch to using groovy to run tests.
    Hard-wires the name of the Spec class.
    Can that be found at runtime?

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -1,8 +1,6 @@
+set -x
 
-groovyc -cp /groovy/spock-core-2.0-M2-groovy-3.0.jar *.groovy
+groovyc -cp ${SPOCK_JAR} *.groovy
 if [ $? -eq 0 ]; then
-  # run test classes even if they are inner classes
-  java -cp .:$(ls /groovy/*.jar | xargs | sed -e 's/ /:/g') \
-      org.junit.runner.JUnitCore \
-        `ls -1 *Spec*.class | sed 's/\(.*\)\..*/\1/'`
+  groovy -cp ${SPOCK_JAR} HikerSpec
 fi
