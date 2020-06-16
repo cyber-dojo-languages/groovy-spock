02431fedce0e3402e6d4f961df78f54c97b3325a
Jon Jagger <jon@jaggersoft.com>
2020-06-16 11:24:51 +0100

    Upgrade to 3.0.3

--- docker/red_amber_green.rb
+++ docker/red_amber_green.rb
@@ -1,16 +1,10 @@
 
 lambda { |stdout,stderr,status|
   output = stdout + stderr
-  green_pattern = Regexp.new('^OK \((\d+) test')
-  if match = green_pattern.match(output)
-    return :green if match[1] != '0'
-    return :red # treat zero passes as a fail
+  pattern = Regexp.new('JUnit5 launcher: passed=(\d+), failed=(\d+)')
+  if match = pattern.match(output)
+    return :red if match[2].to_i > 0
+    return :green
   end
- amber_patterns = [
-    'groovyc: command not found',
-    'groovy\.lang',
-    'MultipleCompilationErrorsException'
-  ]
-  return :amber if amber_patterns.any? { |pattern| Regexp.new(pattern).match(output) }
-  return :red
+  return :amber
 }
--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -1,7 +1,12 @@
-groovyc -cp /groovy/spock-core-1.0-groovy-2.4.jar *.groovy
-if [ $? -eq 0 ]; then
-  # run test classes even if they are inner classes
-  java -cp .:$(ls /groovy/*.jar | xargs | sed -e 's/ /:/g') \
-      org.junit.runner.JUnitCore \
-        `ls -1 *Spec*.class | sed 's/\(.*\)\..*/\1/'`
-fi
+
+# There appears to be no way to pass all the Spec class names
+# to groovy in one go. So I'm calling groovy once for each one.
+# Groovy is so interminably ...sssssslllllllooooooowwwwwww....
+# that you're likely to timeout if you use more than 2 Spec classes!
+# Can you help?
+
+SPECS=($(ls *Spec.groovy | cut -d '.' -f 1))
+for SPEC in "${SPECS[@]}"
+do
+  groovy -cp /groovy/spock-core-2.0-M2-groovy-3.0.jar ${SPEC}
+done
