3f319454c713a1cce6816744ad19ebe209f2ecbc
Faye <faye@kosli.com>
2024-01-17 15:18:57 +0100

    Update base image, jar and regex for testing

--- docker/red_amber_green.rb
+++ docker/red_amber_green.rb
@@ -1,9 +1,9 @@
 
 lambda { |stdout,stderr,status|
   output = stdout + stderr
-  pattern = Regexp.new('JUnit5 launcher: passed=(\d+), failed=(\d+)')
+  pattern = Regexp.new('JUnit5 launcher: passed=(\d+), aborted=(\d+), failed=(\d+)')
   if match = pattern.match(output)
-    return :red if match[2].to_i > 0
+    return :red if match[3].to_i > 0
     return :green
   end
   return :amber
